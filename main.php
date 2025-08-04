8b31d9a885624c754851defb18a85c64fb28eaed
Vu Duc Tai <vutai0122@gmail.com>
2025-08-04 15:45:31 +0700

    sua logic

--- main.php
+++ main.php
@@ -11,51 +11,65 @@
 
     <?php
 
-        class People {
-            public string $name;
-            public int $age;
-            public string $class;
-            private float $defaultWallet = 0;
+    class People
+    {
+        public string $name;
+        public int $age;
+        public string $class;
+        private float $defaultWallet = 0;
 
-            function set_name($name){
-                $this->name = $name;
-            }
-
-            function set_age($age){
-                $this->age = $age;
-            }
-
-            function set_class($class){
-                $this->class = $class;
-            }
-
-            function get_info_student(){
-                echo "THÔNG TIN HỌC SINH<br>";
-                echo "Họ và tên: " . $this->name . "<br>";
-                echo "Tuổi: " . $this->age . "<br>";
-                echo "Lớp: " . $this->class . "<br>";
-            }
-
-            function get_info_teacher(){
-                echo "<br>THÔNG TIN GIẢNG VIÊN" . "<br>";
-                echo "Họ và tên: " . $this->name . "<br>";
-                echo "Tuổi: " . $this->age . "<br>";
-                echo "Lớp: " . $this->class . "<br>";
-                echo "Số tiền ví: " . $this->defaultWallet . " VND<br>";
-            }
+        function set_name($name)
+        {
+            $this->name = $name;
         }
 
-        $student = new People();
-        $student->set_name('Vũ Đức Tài');
-        $student->set_age(22);
-        $student->set_class('Công Nghệ Thông Tin');
-        $student->get_info_student();
+        function set_age($age)
+        {
+            $this->age = $age;
+        }
 
-        $teacher = new People();
-        $teacher->set_name('GIẢNG VIÊN A');
-        $teacher->set_age(40);
-        $teacher->set_class('Công Nghệ Thông Tin');
-        $teacher->get_info_teacher();
+        function set_class($class)
+        {
+            $this->class = $class;
+        }
+
+        function get_info_user()
+        {
+            echo "<br>THÔNG TIN NGƯỜI DÙNG" . "<br>";
+            echo "Họ và tên: " . $this->name . "<br>";
+            echo "Tuổi: " . $this->age . "<br>";
+            echo "Lớp: " . $this->class . "<br>";
+            echo "Số tiền ví: " . $this->defaultWallet . " VND<br>";
+        }
+    }
+
+    class Student extends People
+    {
+        function get_info_student()
+        {
+            echo "THÔNG TIN HỌC SINH<br>";
+            echo "Họ và tên: " . $this->name . "<br>";
+            echo "Tuổi: " . $this->age . "<br>";
+            echo "Lớp: " . $this->class . "<br>";
+        }
+    }
+
+    class Teacher extends People
+    {
+
+    }
+
+    $student = new Student();
+    $student->set_name('Vũ Đức Tài');
+    $student->set_age(22);
+    $student->set_class('Công Nghệ Thông Tin');
+    $student->get_info_student();
+
+    $teacher = new Teacher();
+    $teacher->set_name('GIẢNG VIÊN A');
+    $teacher->set_age(40);
+    $teacher->set_class('Công Nghệ Thông Tin');
+    $teacher->get_info_user();
 
     ?>
 
